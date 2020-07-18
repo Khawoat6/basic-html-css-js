4aef470ab3f64af040f6b824b3e5add49609a75a
oat phattaraphon <phattaraphon.c@ku.th>
2020-07-18 17:05:19 +0700

    js : comparison and logical operators

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-/* Basic JavaScript 
+/* 
+
+    *Basic JavaScript 
 
     โดยส่วนใหญ่่เวลาเราเขียนโค้ดการทำงานอะไรบางอย่าง 
     เราจะต้องกำหนดจุดแสดงผลให้กับมันด้วย เหมือนต้องจี้จุดด้วยว่าจะให้มันไปแสดงผลตรงไหน
     หรือเกิดเหตุการณ์ที่ไหน และยังสามารถรอรับเหตุการณ์ที่จะเกิดขึ้นได้ด้วย
-    
     เช่น กดปุ่มแล้วสั่งให้มันเด้งขึ้นมา etc.
 */
 
@@ -11,7 +12,8 @@
 document.write();
 // Using console.log        แสดงผลลัพธ์ในส่วนของ console ใช้สำหรับ debug
 console.log();
-/* Finding HTML Element
+/* 
+    *Finding HTML Element
     เป็นการไปค้นหา HTML Tag ตามที่เราต้องการ โดยเรามักจะใช้สำหรับการไปดึงคุณสมบัติต่าง ๆ
     ของแท็ก หรือเอาค่าที่เราเขียนใส่กลับเข้าไปในแท็ก
     ที่ใช้บ่อย ๆ ได้แก่
@@ -23,16 +25,19 @@ document.getElementsByClassName("YourClass");
 // 3.  Finding HTML Element by TagName
 document.getElementsByTagName("p");
 
-/* Lenght
-  .length          ใช้หาความยาวของ Array หรือความยาวของ String ได้เช่นกัน
+/* 
+    *Lenght
+    .length          ใช้หาความยาวของ Array หรือความยาวของ String ได้เช่นกัน
 */
 
-/*  Convert to String, Convert to Integer 
+/*  
+    *Convert to String, Convert to Integer 
     .toString();     ใช้สำหรับแปลงเป็น String
     parseInt();      ใช้สำหรับแปลงเป็น Integer
 */
 
-/* JavaScript Operators
+/* 
+    *JavaScript Operators
 
     1.  Arithmetic Operators
     2.  Assighment Operators
@@ -49,13 +54,29 @@ document.getElementsByTagName("p");
     6.     ++           Increment
     7.     --           Decrement
 
-   
-  
+   *Assighment Operators
+    1.      =       x = y       x = y
+    2.      +=      x += y      x = x + y
+    3.      -=      x -= y      x = x - y
+    4.      *=      x *= y      x = x * y
+    5.      /=      x /= y      x = x / y
+    6.      %=      x %= y      x = x % y
+    
+   *Comparison and Logical Operators
+    1.      ==      equal to
+    2.      ===     equal value and equal type
+    3.      !=      not equal
+    4.      !===    not equal value or not equal type
+    5.      >       greater than
+    6.      <       less than
+    7.      >=      greater than or equal to
+    8.      <=      less than or equal to
+    9.      &&      and
+    10.     ||      or
+    11.     !       not
 
 
 
 
 
 */
-
-
